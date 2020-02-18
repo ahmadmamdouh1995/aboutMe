59ca07a59cfc851330a14f91eeffdab96bcbc0e2
dianaazzam <dianaazzam@DESKTOP-9F18RBI.localdomain>
2020-02-18 10:24:42 +0200

    formatted file

--- js/app.js
+++ js/app.js
@@ -1,95 +1,95 @@
 "use strict"
-var yourName=prompt("Hi! What's your name?");
+var yourName = prompt("Hi! What's your name?");
 //console.log(yourName);
-alert("Hey "+yourName+"!");
+alert("Hey " + yourName + "!");
 
-var myAge=prompt("Do you think my age is above 25?")
-switch(myAge.toLowerCase()) {
+var myAge = prompt("Do you think my age is above 25?")
+switch (myAge.toLowerCase()) {
     case "no":
     case "n":
         //console.log("You're right! my age is below 25.");
         alert("You're right! my age is below 25.");
-    break;
+        break;
 
     case "yes":
     case "y":
         //console.log("Opps.. close enough. I'm still 24");
         alert("Opps.. close enough but I'm still 24");
-    break;
+        break;
 }
 
 
-var myHobby=prompt("Do you think I like drawing?")
-switch(myHobby.toLowerCase()) {
+var myHobby = prompt("Do you think I like drawing?")
+switch (myHobby.toLowerCase()) {
     case "no":
     case "n":
         //console.log("That's kinda true.. it's a love-hate relationship");
         alert("That's kinda true .. it's a love-hate relationship");
-    break;
+        break;
 
     case "yes":
     case "y":
         //console.log("You're right! I like drawing but I hate it at the same time :(");
         alert("You're right! I like drawing but I hate it at the same time :(");
-    break;
+        break;
 
 
 }
 
-var music=prompt("Do you think I like music?")
-switch(music.toLowerCase()) {
+var music = prompt("Do you think I like music?")
+switch (music.toLowerCase()) {
     case "no":
     case "n":
         //console.log("You're not right. I do like music!");
         alert("You're not right. I do like music!");
-    break;
+        break;
 
     case "yes":
     case "y":
         //console.log("That's right! I like all kinds of music!");
         alert("That's right! I like all kinds of music!");
-    break;
+        break;
 
-    default: 
-    alert("Only yes or no answers");
-        
+    default:
+        alert("Only yes or no answers");
+
 
 }
 
-var sing=prompt("Do you think I could sing?")
-switch(sing.toLowerCase()) {
+var sing = prompt("Do you think I could sing?")
+switch (sing.toLowerCase()) {
     case "no":
     case "n":
 
         //console.log("Correct. I wish I could.");
         alert("Correct but I wish I could.");
-    break;
+        break;
 
     case "yes":
     case "y":
 
         //console.log("That' incorrect but I wish I could!");
         alert("That' incorrect but I wish I could!");
-    break;
-        
+        break;
+
 
 }
 
-var family=prompt("Do you think I am an only child?")
-switch(music.toLowerCase()) {
+var family = prompt("Do you think I am an only child?")
+switch (music.toLowerCase()) {
     case "no":
     case "n":
         //console.log("That's true. I have 4 sisters and one brother.");
         alert("That's true. I have 4 sisters and one brother.");
-    break;
+        break;
 
     case "yes":
     case "y":
         //console.log("That's wrong. I have 4 sisters and one brother.");
         alert("That's wrong. I have 4 sisters and one brother.");
-    break;   
-    
+        break;
+
 
 }
 
-alert("Thanks "+yourName+" Make sure to comeback, I'll keep updating the Website!");
+alert("Thanks " + yourName + " Make sure to comeback, I'll keep updating the Website!");
